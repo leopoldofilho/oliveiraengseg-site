18f33e782faca11115c0ea69622f68545e2babed
leopoldofilho <leopoldo.oliveira@gmail.com>
2025-10-19 12:28:34 -0300

    Delete whatsapp.ts

--- whatsapp.ts
+++
@@ -1,5 +0,0 @@
-export const buildWhatsAppLink = (phoneE164: string, text: string) => {
-  const p = phoneE164.replace(/[^0-9]/g, "");
-  const t = encodeURIComponent(text);
-  return `https://wa.me/${p}?text=${t}`;
-};
